2a3d85dd2610fbd4e5092f2233a9b971d5ac642e
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2014-12-21 20:06:11 +0800

    Initial Commit

---
+++ Postcard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Postcard
+//
+//  Created by Jonathan Poltak Samosir on 21/12/2014.
+//  Copyright (c) 2014 Jonathan Poltak Samosir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
